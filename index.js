4d54ece16ad574d54449c22e98f667a2488d0207
Joy Olago <joydanielle62@gmail.com>
2023-06-19 15:41:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+let activity;
+
+function saturdayFun(activity = "roller-skate"){
+        return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun(activity))
+
+
+function mondayWork(activity = "go to the office"){
+        return `This Monday, I will ${activity}.`
+    }
+    console.log(mondayWork())
+
+    let initialization;
+    let word;
+
+    function wrapAdjective(initialization ='*'){
+        return function(adjective = special) {
+            return `You are ${initialization}${word}${initialization}!`
+        }
+    }
+    console.log(wrapAdjective(initialization))
